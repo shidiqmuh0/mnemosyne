c1b307ca9d0b3313a6f87f5440df4790ed81c3b7
shidiqmuh0 <57977381+shidiqmuh0@users.noreply.github.com>
2023-06-09 13:52:07 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
 
+menuToggle.addEventListener('click', function () {
+    nav.classList.toggle('slide');
+});
